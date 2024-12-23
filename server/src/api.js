02073600e505044a3652cdb005f27c53e60324ee
Hancel.Lin <imlinhanchao@gmail.com>
2024-12-23 18:03:15 +0800

    feat: 添加查询群聊接口

--- server/src/api.js
+++ server/src/api.js
@@ -1,9 +1,56 @@
 const { User } = require('./db');
 const crypto = require('crypto');
 
+async function contactToJson (contact) {
+  return {
+    alias: await contact.alias(),
+    avatarUrl: await contact.avatar(),
+    name: contact.name(),
+    type: contact.type(),
+    gender: contact.gender(),
+    province: contact.province(),
+    city: contact.city(),
+    self: contact.self(),
+    id: contact._wxid
+  }
+}
+
+function roomToJson (room) {
+  return {
+    alias: room.name || room.remark || room.nickName,
+    avatarUrl: room.avatarImg || room.smallHeadImgUrl,
+    name: room.name || room.nickName,
+    type: 2,
+    id: room.chatroomId,
+    room: { ...room },
+  }
+}
+
+
+
 const msgList = []
-function createRouter(bot, router) {
+function createRouter (bot, router, wss) {
   const user = new User();
+
+  function queryTarget({ id, isRoom }) {
+    if (!isRoom && !id.endsWith('@chatroom')) {
+      return bot.Contact({ id });
+    } else {
+      return new bot.Room(bot.db.findOneByChatroomId(id));
+    }
+  }
+
+  function verifyToken (ctx) {
+    const token = ctx.request.header.authorization;
+    if (!token || !user.find(token)) {
+      ctx.body = {
+        code: 401,
+        message: '未授权'
+      }
+      return false;
+    }
+    return true;
+  }
 
   if (!user.isInit) {
     router.get('/init', async (ctx) => {
@@ -26,6 +73,13 @@ function createRouter(bot, router) {
       }
     });
   }
+
+  router.get('/emoji', async (ctx) => {
+    const url = ctx.query.url;
+    await fetch(url).then(res => res.blob()).then(blob => blob.arrayBuffer()).then(buffer => {
+      ctx.body = Buffer.from(buffer)
+    })
+  })
 
   router.get('/inited', async (ctx) => {
     ctx.body = {
@@ -61,24 +115,36 @@ function createRouter(bot, router) {
     };
   });
 
-  function verifyToken(ctx) {
-    const token = ctx.request.header.authorization;
-    if (!token || !user.find(token)) {
-      ctx.body = {
-        code: 401,
-        message: '未授权'
-      }
-      return false;
-    }
-    return true;
-  }
-
   router.post('/chat/send', async (ctx) => {
     const body = ctx.request.body;
     const { text, ...params } = body;
-    const contact = await bot.Contact.find(params);
+    const contact = await queryTarget(params);
     const msg = await contact.say(text);
+    const info = await bot.info();
     msgList.push(msg);
+    let room = false;
+    if (contact.chatroomId) {
+      room = await roomToJson(contact)
+    }
+    wss.send({
+      type: bot.Message.Type.Text,
+      id: msg.newMsgId.toString(),
+      data: text,
+      from: {
+        alias: info.nickName,
+        avatarUrl: info.bigHeadImgUrl,
+        name: info.nickName,
+        type: 1,
+        gender: info.sex,
+        province: info.province,
+        city: info.city,
+        self: true,
+        id: info.wxid
+      },
+      in: room || await contactToJson(contact),
+      isRoom: !!room,
+      self: true,
+    });
     setTimeout(() => {
       msgList.splice(msgList.indexOf(msg), 1);
     }, 1000 * 60 * 5);
@@ -88,34 +154,31 @@ function createRouter(bot, router) {
       data: msg,
     }
   })
+  router.get('/chat/rooms', async (ctx) => {
+    const rooms = bot.db.findAllRooms();
+    ctx.body = {
+      code: 200,
+      data: rooms
+    }
+  })
   router.get('/chat/contacts', async (ctx) => {
     const contacts = await bot.Contact.findAll();
     ctx.body = {
       code: 200,
-      data: await Promise.all(contacts.map(async c => ({
-        alias: await c.alias(),
-        avatarUrl: await c.avatar(),
-        name: c.name(),
-        type: c.type(),
-        gender: c.gender(),
-        province: c.province(),
-        city: c.city(),
-        self: c.self(),
-        id: c._wxid 
-      })))
+      data: (await Promise.all(contacts.map(contactToJson))).concat(bot.db.findAllRooms().map(roomToJson))
     }
-  })  
+  })
   router.get('/chat/contact', async (ctx) => {
     const query = ctx.request.query;
     const contact = await bot.Contact.find(query);
     ctx.body = {
       code: 200,
-      data: contact
+      data: await contactToJson(contact)
     }
   })
   router.post('/chat/revoke', async (ctx) => {
     const body = ctx.request.body;
-    const msg = msgList.find(m => m.msgId === body.id);
+    const msg = msgList.find(m => m.newMsgId === body.id);
     if (!msg) {
       return ctx.body = {
         code: 404,
@@ -144,5 +207,7 @@ function createRouter(bot, router) {
 
 module.exports = {
   createRouter,
-  msgList
+  msgList,
+  contactToJson,
+  roomToJson,
 }
--- server/src/index.js
+++ server/src/index.js
@@ -28,42 +28,80 @@ const bot = new GeweBot({
 
 // 监听消息事件
 const onMessage = async (msg) => {
+  let from = await msg.from();
+  from = {
+    alias: await from.alias(),
+    avatarUrl: await from.avatar(),
+    name: from.name(),
+    type: from.type(),
+    gender: from.gender(),
+    province: from.province(),
+    city: from.city(),
+    self: from.self(),
+    id: from._wxid 
+  }
+  let room = await msg.room();
+  if (room) {
+    room = {
+      alias: room.name,
+      avatarUrl: room.avatarImg,
+      name: room.name,
+      type: 2,
+      id: room.chatroomId,
+      room: { ...room },
+    }
+  }
   // 处理消息...
+  const base = {
+    id: msg._msgId,
+    from,
+    in: room || from,
+    isRoom: !!room,
+    self: await msg.self(),
+    mentionSelf: await msg.mentionSelf(),
+    date: msg.date(),
+    age: msg.age(),
+    source: msg.getXmlToJson?.(),
+  }
   // 回复文本消息
   if (msg.type() === bot.Message.Type.Text) { //类型详见 MessageType 表
     //await msg.say("Hello, World!");
-    wss.send({ type: bot.Message.Type.Text, data: msg.text() });
+    wss.send({ type: bot.Message.Type.Text, data: msg.text(), ...base });
   }
   // 处理图片消息
   else if (msg.type() === bot.Message.Type.Image) {
-    const filebox = await msg.toFileBox();
-    const filePath = path.join(imgStatic, filebox.name);
-    const fileUrl = `./img/${filebox.name}`;
-    filebox.toFile(filePath);
-    wss.send({ type: bot.Message.Type.Image, data: fileUrl });
+    try {
+      const filebox = await msg.toFileBox();
+      const filePath = path.join(imgStatic, filebox.name);
+      const fileUrl = `./img/${filebox.name}`;
+      filebox.toFile(filePath);
+      wss.send({ type: bot.Message.Type.Image, data: fileUrl, ...base });
+    } catch(e) {
+      wss.send({ type: bot.Message.Type.Image, data: `./default.jpg`, ...base });
+    }
   }
   // 处理文件消息
   else if (msg.type() === bot.Message.Type.File) {
     const filebox = await msg.toFileBox();
     if (!filebox) {
       const data = bot.Message.getXmlToJson(msg.text());
-      wss.send({ type: msg.type(), data });
+      wss.send({ type: msg.type(), data, ...base });
       return;
     }
     const filePath = path.join(fileStatic, filebox.name);
     const fileUrl = `./file/${filebox.name}`;
     filebox.toFile(filePath);
-    wss.send({ type: bot.Message.Type.File, data: fileUrl });
+    wss.send({ type: bot.Message.Type.File, data: fileUrl, ...base });
   }
   else if (msg.type() === bot.Message.Type.Emoji) {
     //await msg.say("收到表情");
     const data = bot.Message.getXmlToJson(msg.text());
     console.log(data.msg.emoji.cdnurl);
-    wss.send({ type: bot.Message.Type.Emoji, data: data.msg.emoji.cdnurl });
+    wss.send({ type: bot.Message.Type.Emoji, data: data.msg.emoji.cdnurl, ...base });
   }
   else {
     const data = bot.Message.getXmlToJson(msg.text());
-    wss.send({ type: msg.type(), data });
+    wss.send({ type: msg.type(), data, ...base });
   }
 };
 
@@ -80,7 +118,7 @@ bot.on('all', msg => { // 如需额外的处理逻辑可以监听 all 事件 该
 bot
   .start()
   .then(async ({app, router}) => {
-    createRouter(bot, router) // 创建路由
+    createRouter(bot, router, wss) // 创建路由
     app.use(router.routes()).use(router.allowedMethods());
     
   })
