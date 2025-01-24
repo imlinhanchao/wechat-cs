ec3a2c821bfe2c88bd79b0a7b82341a133ae0338
lin.hanchao <lin.hanchao@byd.com>
2025-01-24 09:45:07 +0800

    🎈 perf: 优化emoji样式

--- extension/webview/src/views/home/components/emoji.vue
+++ extension/webview/src/views/home/components/emoji.vue
@@ -1,28 +1,42 @@
 <script setup lang="ts">
 import Icon from '@/components/Icon';
+import { useEventListener } from '@/hooks/useEventListener';
 import { defaultEmojis } from '@/utils/emoji';
 import { ref } from 'vue';
 const emit = defineEmits(['input']);
 
 const visible = ref(false);
+
+useEventListener({
+  el: document.body,
+  name: 'click',
+  listener: (ev) => {
+    const target: Element = ev.target as Element;
+    if (target.closest('.emoji-list') || target.closest('.emoji-btn')) return;
+    visible.value = false;
+  }
+});
 </script>
 
 <template>
   <Teleport to="body">
-    <div v-show="visible" class="absolute right-2 left-60% top-2 bottom-3em overflow-auto">
-      <section class="grid grid-cols-10 gap-1">
+    <div
+      v-show="visible"
+      class="absolute right-2 top-2 bottom-2.5em overflow-auto bg-[#313131] rounded-lg p-[2px] emoji-list"
+    >
+      <section class="grid grid-cols-10 gap-[2px]">
         <div
           v-for="emoji in defaultEmojis"
           :key="emoji"
           @click="(emit('input', emoji), (visible = false))"
-          class="flex items-center justify-center w-5 h-5 rounded-lg bg-[#1f1f1f] cursor-pointer"
+          class="flex items-center justify-center w-6 h-6 rounded bg-[#1f1f1f] cursor-pointer p-[3px]"
         >
-          <img :src="`./emoji/${emoji}.png`" class="w-6 h-6 no-preview" />
+          <img :src="`./emoji/${emoji}.png`" class="w-full h-full no-preview" />
         </div>
       </section>
     </div>
   </Teleport>
-  <el-button type="primary" link @click="visible = !visible">
+  <el-button type="primary" link @click="visible = !visible" class="emoji-btn">
     <Icon icon="mingcute:emoji-2-fill" class="text-[#dcdcaa]" />
   </el-button>
 </template>
